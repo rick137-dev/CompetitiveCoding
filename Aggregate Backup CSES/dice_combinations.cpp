1fa436bb6ec42450e5f3e13b03ede19f107be04a
rick137-dev <caiulorick137@gmail.com>
2025-06-19 02:23:18 +0100

    Added extra solution

---
+++ Aggregate Backup CSES/dice_combinations.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <set>
+#include <algorithm>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+int mod = 1e9 + 7;
+
+long long diceCombs(int N, vector<int> &dp)
+{
+       if (N < 0)
+              return 0;
+       if (dp[N] != -1)
+              return dp[N];
+       long long total = 0;
+       for (int i = 1; i <= 6; i++)
+       {
+              total = (total % mod + diceCombs(N - i, dp) % mod) % mod;
+       }
+       return dp[N] = total;
+}
+int main()
+{
+       int N;
+       cin >> N;
+       vector<int> dp(N + 1, -1);
+       dp[0] = 1;
+       dp[1] = 1;
+       dp[2] = 2;
+       cout << diceCombs(N, dp) % mod;
+
+       return 0;
+}
--- secondMain.cpp
+++ secondMain.cpp
@@ -11,21 +11,10 @@
 
 using namespace std;
 
+int mod = 1e9 + 7;
+
 int main()
 {
-       int n, m;
-       cin >> n >> m;
-
-       int a, b;
-       unordered_map<int, vector<int>> Alist;
-
-       for (int i = 0; i < m; i++)
-       {
-              cin >> a >> b;
-              Alist[a - 1].push_back(b - 1);
-              Alist[b - 1].push_back(a - 1);
-       }
-       vector<int> previous(n, -1);
 
        return 0;
 }
