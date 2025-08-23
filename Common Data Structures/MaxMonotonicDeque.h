2d395201886ffc28a5ca63a5e1dbf5a096ef1dde
rick137-dev <caiulorick137@gmail.com>
2025-08-23 23:44:39 +0200

    Added header files for implementations of common data structures

---
+++ Common Data Structures/MaxMonotonicDeque.h
@@ -0,0 +1,34 @@
+#include <deque>
+#include <stdexcept>
+
+using namespace std;
+
+template <typename T>
+class MonotonicDeque
+{
+private:
+    deque<T> myQueue;
+
+public:
+    T getMinimum()
+    {
+
+        return myQueue.front();
+    }
+    void removeNumber(T num)
+    {
+        if (myQueue.size() > 0 && myQueue.front() == num)
+        {
+            myQueue.pop_front();
+        }
+    }
+
+    void insertNumber(T number)
+    {
+        while (myQueue.size() > 0 && myQueue.back() < number)
+        {
+            myQueue.pop_back();
+        }
+        myQueue.push_back(number);
+    }
+};
